b1349c4d2bd4b95003d5d00fec2aea31728d67bd
dengshihong <dshnightmare@gmail.com>
2019-08-23 12:08:19 +0800

    Update news_crawlerYX.py

--- crawler/news_crawlerYX.py
+++ crawler/news_crawlerYX.py
@@ -34,13 +34,14 @@ def wait_for_page_load(self, timeout=30):
     yield
     WebDriverWait(self.browser, timeout).until(EC.staleness_of(old_page))
 
-
 # options = Options()
 # options.headless = True
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--log-level=3')
+options.add_argument("--disable-extensions")
+options.add_argument("--proxy-server=socks5://127.0.0.1:1080")
 titles = []
 abstracts = []
 urls = []
@@ -54,8 +55,8 @@ def run(start, end, file):
     if end == -1:
         end = len(data)
     print(start, end)
-    with open('../data/download{}-{}.csv'.format(start, end), 'w', encoding='UTF-8') as csvfile,\
-            open('../data/untreated{}-{}'.format(start, end), 'w', encoding='UTF-8') as undo:
+    with open('../data/{}_download{}-{}.csv'.format(file, start, end), 'w', encoding='UTF-8') as csvfile,\
+            open('../data/{}_untreated{}-{}'.format(file, start, end), 'w', encoding='UTF-8') as undo:
         fieldnames = ['title', 'url', 'newsurl', 'abstract', 'text_body']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL)
         writer.writeheader()
@@ -63,9 +64,16 @@ def run(start, end, file):
         driver = webdriver.Chrome(chrome_options=options, executable_path='../bin/chromedriver.exe')
         # driver = driver = webdriver.Firefox(options=options, executable_path='C:\pythonpath\geckodriver.exe')
         for index, lines in enumerate(data[start:end]):
-            title = lines.split('\t')[4]
-            url = lines.split('\t')[5]
-            abstract = lines.split('\t')[6]
+            csvfile.flush()
+            undo.flush()
+            # if index < 24:
+            #     continue
+            if file == 'Train':
+                _, _, _, _, title, url, abstract = lines.split('\t')
+            elif file == 'Test':
+                _, _, _, title, url, abstract = lines.split('\t')
+            else:
+                raise  ValueError
             '''
             1. twitter url + news url already converted to short url
             2. twitter url + news url not handle
@@ -74,45 +82,85 @@ def run(start, end, file):
             '''
             print('{}: {}'.format(start + index, url))
             pattern1 = re.compile('https://twitter.com/.+/status/[0-9]+')
+            newsurl = None
             if pattern1.match(url):
-                pattern2 = re.compile('https://t.co/[a-zA-Z0-9]+')
-                result = re.search(pattern2, abstract)
-                if result != None:
-                    newsurl = result.group(0)
-                else:
+                # 不使用提供的短网址
+                # pattern2 = re.compile('https://t.co/[a-zA-Z0-9]+')
+                # result = re.search(pattern2, abstract)
+                # Quote Tweet
+                while True:
                     try:
                         driver.get(url)
-                        try:
-                            text = driver.find_element_by_class_name('js-tweet-text-container')
-                            link = text.find_element_by_class_name('twitter-timeline-link')
-                            newsurl = link.get_attribute('href')
-                        except NoSuchElementException as e:
-                            print('no link in text: {}'.format(str(e)))
-                            try:
-                                media = driver.find_element_by_class_name('card2 js-media-container')
-                                type = media.get_attribute('data-card2-name')
-                                if type in []:
-                                    raise ValueError
-                                link = media.find_element_by_tag_name('a')
-                                newsurl = link.get_attribute('href')
-                            except NoSuchElementException as e:
-                                print('no card link: {}'.format(str(e)))
-                                print("<<<<<<<<<<<<no newsurl>>>>>>>>>>>")
-                                writer.writerow({
-                                    'title': title,
-
-                                    'url': url,
-
-                                    'newsurl': "none",
-
-                                    'abstract': abstract,
-
-                                    'text_body': "none"
-                                })
-                                undo.write('pure text:{}\n'.format(index))
-                                continue
+                        WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, "html")))
+                        tweet = driver.find_elements_by_class_name('QuoteTweet')
+                        media = tweet.find_element_by_class_name('js-media-container')
+                        item = media.get_attribute('href')
+                        assert item is not None
+                        _url = 'https://twitter.com{}'.format(item)
+                        print(_url)
+                        if url.split('/')[-1] == _url.split('/')[-1]:
+                            break
+                        url = _url
                     except Exception as e:
-                        print(e)
+                        break
+                try:
+                    text = driver.find_element_by_class_name('js-tweet-text-container')
+                except Exception as e:
+                    print('invalid tweet: {}'.format(str(e)))
+                    writer.writerow({
+                        'title': title,
+
+                        'url': url,
+
+                        'newsurl': "none",
+
+                        'abstract': abstract,
+
+                        'text_body': "none"
+                    })
+                    undo.write('invalid tweet:{}\n'.format(index))
+                    continue
+                # find card
+                try:
+                    media = driver.find_element_by_class_name('js-media-container')
+                    type = media.get_attribute('data-card2-name')
+                    if type in ['summary_large_image', 'summary']:
+                        newsurl = media.find_element_by_tag_name('div').get_attribute('data-card-url')
+                    elif type in ['player', None]:
+                        raise NoSuchElementException
+                    elif type.find('choice_text_only'):
+                        raise NoSuchElementException
+                    elif type.find('message_me') != -1:
+                        raise NoSuchElementException
+                    elif type.find('moment') != -1:
+                        raise NoSuchElementException
+                    else:
+                        raise ValueError
+                except ValueError as e:
+                    raise
+                except NoSuchElementException as e:
+                    print('no card element: {}'.format(str(e)))
+                    # find link in text
+                    links = text.find_elements_by_class_name('twitter-timeline-link')
+                    hides = text.find_elements_by_class_name('u-hidden')
+                    for link in links:
+                        if link not in hides:
+                            newsurl = link.get_attribute('href')
+                            break
+                    if newsurl is None:
+                        print('no link in text: {}'.format(str(e)))
+                        writer.writerow({
+                            'title': title,
+
+                            'url': url,
+
+                            'newsurl': "none",
+
+                            'abstract': text.text,
+
+                            'text_body': "none"
+                        })
+                        continue
             else:
                 newsurl = url
             print(newsurl)
@@ -122,8 +170,8 @@ def run(start, end, file):
                 try:
                     driver.get(newsurl)
                     print("<<<<<<<<<<<<Waiting>>>>>>>>>>>")
-                    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.TAG_NAME, "html")))
-                except TimeoutException as e:
+                    WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.TAG_NAME, "html")))
+                except Exception as e:
                     num += 1
                     print("<<<<<<<<<<<<loading error {}>>>>>>>>>>>".format(num))
                     if num < 3:
@@ -148,21 +196,27 @@ def run(start, end, file):
                 continue
             longurl = driver.current_url
             print(longurl)
-            if pattern1.match(longurl):
-                print("<<<<<<<<<<<<Quete Tweet>>>>>>>>>>>")
-                writer.writerow({
-                    'title': title,
-
-                    'url': url,
-
-                    'newsurl': newsurl,
-
-                    'abstract': abstract,
-
-                    'text_body': "none"
-                })
-                undo.write('quote tweet:{}\n'.format(index))
-                continue
+            # if pattern1.match(longurl):
+            #     print("<<<<<<<<<<<<Quete Tweet>>>>>>>>>>>")
+            #     assert longurl.split('/')[-1] == url.split('/')[-1]
+            #     try:
+            #         container = driver.find_element_by_class_name('permalink-tweet-container')
+            #         text = container.find_element_by_class_name('js-tweet-text-container')
+            #     except NoSuchElementException as e:
+            #         raise
+            #     writer.writerow({
+            #         'title': title,
+            #
+            #         'url': url,
+            #
+            #         'newsurl': newsurl,
+            #
+            #         'abstract': abstract,
+            #
+            #         'text_body': text.text
+            #     })
+            #     # undo.write('quote tweet:{}\n'.format(index))
+            #     continue
             try:
                 article = Article(longurl)
                 article.download()
